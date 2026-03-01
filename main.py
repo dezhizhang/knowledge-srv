e1d8124bcc893cd1160999dc61e314e2af004d16
dezhizhang <1541609448@qq.com>
2026-03-01 17:57:20 +0800

    feat:项目结构的拆分

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from knowledge-srv!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.api import router
 
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+)
+
+app.include_router(router)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
